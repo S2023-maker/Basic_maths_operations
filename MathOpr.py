624bf6b9673719b402524a64ed9968a87ed00872
S2023-maker <shreyarjoshi1995@gmail.com>
2025-03-30 16:00:40 +0530

    Add files via upload

---
+++ MathOpr.py
@@ -0,0 +1,13 @@
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+
+print(f"Addition: {num1} + {num2} = {addition}")
+print(f"Subtraction: {num1} - {num2} = {subtraction}")
+print(f"Multiplication: {num1} * {num2} = {multiplication}")
+print(f"Division: {num1} / {num2} = {division}")
